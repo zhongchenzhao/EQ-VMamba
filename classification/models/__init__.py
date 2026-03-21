03fce880f666a1db7ee6056e07b2346341138929
ZhongchenZhao <47920208+zhongchenzhao@users.noreply.github.com>
2026-03-21 19:43:48 +0800

    Add files via upload

--- classification/models/__init__.py
+++ classification/models/__init__.py
@@ -1,13 +1,5 @@
-import os
-from functools import partial
-import torch
-
 from .vmamba import VSSM
 from .eq_vmamba_light import EQVSSM
-from .eq_vmamba_sharedLinear_light import EQVSSM_sharedLinear
-from .eq_vmamba_Bconv_light import EQVSSMBconv
-from .eq_vmamba_Fconv_repeat_light import EQVSSMRepeat
-from .vmamba_eqscan_light import VSSMEQScan
 
 
 def build_vssm_model(config, **kwargs):
@@ -49,42 +41,6 @@ def build_vssm_model(config, **kwargs):
         return model
 
 
-    elif model_type in ["vssm_eqscan"]:
-        model = VSSMEQScan(
-            patch_size=config.MODEL.VSSM.PATCH_SIZE,
-            in_chans=config.MODEL.VSSM.IN_CHANS,
-            num_classes=config.MODEL.NUM_CLASSES,
-            depths=config.MODEL.VSSM.DEPTHS,
-            dims=config.MODEL.VSSM.EMBED_DIM,
-            # ===================
-            ssm_d_state=config.MODEL.VSSM.SSM_D_STATE,
-            ssm_ratio=config.MODEL.VSSM.SSM_RATIO,
-            ssm_rank_ratio=config.MODEL.VSSM.SSM_RANK_RATIO,
-            ssm_dt_rank=("auto" if config.MODEL.VSSM.SSM_DT_RANK == "auto" else int(config.MODEL.VSSM.SSM_DT_RANK)),
-            ssm_act_layer=config.MODEL.VSSM.SSM_ACT_LAYER,
-            ssm_conv=config.MODEL.VSSM.SSM_CONV,
-            ssm_conv_bias=config.MODEL.VSSM.SSM_CONV_BIAS,
-            ssm_drop_rate=config.MODEL.VSSM.SSM_DROP_RATE,
-            ssm_init=config.MODEL.VSSM.SSM_INIT,
-            forward_type=config.MODEL.VSSM.SSM_FORWARDTYPE,
-            # ===================
-            mlp_ratio=config.MODEL.VSSM.MLP_RATIO,
-            mlp_act_layer=config.MODEL.VSSM.MLP_ACT_LAYER,
-            mlp_drop_rate=config.MODEL.VSSM.MLP_DROP_RATE,
-            # ===================
-            drop_path_rate=config.MODEL.DROP_PATH_RATE,
-            patch_norm=config.MODEL.VSSM.PATCH_NORM,
-            norm_layer=config.MODEL.VSSM.NORM_LAYER,
-            downsample_version=config.MODEL.VSSM.DOWNSAMPLE,
-            patchembed_version=config.MODEL.VSSM.PATCHEMBED,
-            gmlp=config.MODEL.VSSM.GMLP,
-            use_checkpoint=config.TRAIN.USE_CHECKPOINT,
-            # ===================
-            posembed=config.MODEL.VSSM.POSEMBED,
-            imgsize=config.DATA.IMG_SIZE,
-        )
-        return model
-
 
     elif model_type in ["eqvssm"]:
         model = EQVSSM(
@@ -123,129 +79,12 @@ def build_vssm_model(config, **kwargs):
         )
         return model
 
-    elif model_type in ["eqvssm_sharedLinear", "eqvssm_sharedlinear"]:
-        model = EQVSSM_sharedLinear(
-            patch_size=config.MODEL.VSSM.PATCH_SIZE,
-            in_chans=config.MODEL.VSSM.IN_CHANS,
-            num_classes=config.MODEL.NUM_CLASSES,
-            depths=config.MODEL.VSSM.DEPTHS,
-            dims=config.MODEL.VSSM.EMBED_DIM,
-            # ===================
-            ssm_d_state=config.MODEL.VSSM.SSM_D_STATE,
-            ssm_ratio=config.MODEL.VSSM.SSM_RATIO,
-            ssm_rank_ratio=config.MODEL.VSSM.SSM_RANK_RATIO,
-            ssm_dt_rank=("auto" if config.MODEL.VSSM.SSM_DT_RANK == "auto" else int(config.MODEL.VSSM.SSM_DT_RANK)),
-            eq_tranNum=config.MODEL.VSSM.EQ_tranNum,            # 4
-            ssm_act_layer=config.MODEL.VSSM.SSM_ACT_LAYER,
-            ssm_conv=config.MODEL.VSSM.SSM_CONV,
-            ssm_conv_bias=config.MODEL.VSSM.SSM_CONV_BIAS,
-            ssm_drop_rate=config.MODEL.VSSM.SSM_DROP_RATE,
-            ssm_init=config.MODEL.VSSM.SSM_INIT,
-            forward_type=config.MODEL.VSSM.SSM_FORWARDTYPE,
-            # ===================
-            mlp_ratio=config.MODEL.VSSM.MLP_RATIO,
-            mlp_act_layer=config.MODEL.VSSM.MLP_ACT_LAYER,
-            mlp_drop_rate=config.MODEL.VSSM.MLP_DROP_RATE,
-            # ===================
-            drop_path_rate=config.MODEL.DROP_PATH_RATE,         # 0.2
-            patch_norm=config.MODEL.VSSM.PATCH_NORM,
-            norm_layer=config.MODEL.VSSM.NORM_LAYER,
-            downsample_version=config.MODEL.VSSM.DOWNSAMPLE,
-            patchembed_version=config.MODEL.VSSM.PATCHEMBED,
-            gmlp=config.MODEL.VSSM.GMLP,
-            use_checkpoint=config.TRAIN.USE_CHECKPOINT,
-            # ===================
-            posembed=config.MODEL.VSSM.POSEMBED,
-            imgsize=config.DATA.IMG_SIZE,
-        )
-        return model
-
-
-    elif model_type in ["eqvssm_bconv"]:
-        model = EQVSSMBconv(
-            patch_size=config.MODEL.VSSM.PATCH_SIZE,
-            in_chans=config.MODEL.VSSM.IN_CHANS,
-            num_classes=config.MODEL.NUM_CLASSES,
-            depths=config.MODEL.VSSM.DEPTHS,
-            dims=config.MODEL.VSSM.EMBED_DIM,
-            # ===================
-            ssm_d_state=config.MODEL.VSSM.SSM_D_STATE,
-            ssm_ratio=config.MODEL.VSSM.SSM_RATIO,
-            ssm_rank_ratio=config.MODEL.VSSM.SSM_RANK_RATIO,
-            ssm_dt_rank=("auto" if config.MODEL.VSSM.SSM_DT_RANK == "auto" else int(config.MODEL.VSSM.SSM_DT_RANK)),
-            eq_tranNum=config.MODEL.VSSM.EQ_tranNum,            # 4
-            ssm_act_layer=config.MODEL.VSSM.SSM_ACT_LAYER,
-            ssm_conv=config.MODEL.VSSM.SSM_CONV,
-            ssm_conv_bias=config.MODEL.VSSM.SSM_CONV_BIAS,
-            ssm_drop_rate=config.MODEL.VSSM.SSM_DROP_RATE,
-            ssm_init=config.MODEL.VSSM.SSM_INIT,
-            forward_type=config.MODEL.VSSM.SSM_FORWARDTYPE,
-            # ===================
-            mlp_ratio=config.MODEL.VSSM.MLP_RATIO,
-            mlp_act_layer=config.MODEL.VSSM.MLP_ACT_LAYER,
-            mlp_drop_rate=config.MODEL.VSSM.MLP_DROP_RATE,
-            # ===================
-            drop_path_rate=config.MODEL.DROP_PATH_RATE,         # 0.2
-            patch_norm=config.MODEL.VSSM.PATCH_NORM,
-            norm_layer=config.MODEL.VSSM.NORM_LAYER,
-            downsample_version=config.MODEL.VSSM.DOWNSAMPLE,
-            patchembed_version=config.MODEL.VSSM.PATCHEMBED,
-            gmlp=config.MODEL.VSSM.GMLP,
-            use_checkpoint=config.TRAIN.USE_CHECKPOINT,
-            # ===================
-            posembed=config.MODEL.VSSM.POSEMBED,
-            imgsize=config.DATA.IMG_SIZE,
-        )
-        return model
-
-
-    elif model_type in ["eqvssm_repeat", "eqvssm_fconv_repeat"]:
-        model = EQVSSMRepeat(
-            patch_size=config.MODEL.VSSM.PATCH_SIZE,
-            in_chans=config.MODEL.VSSM.IN_CHANS,
-            num_classes=config.MODEL.NUM_CLASSES,
-            depths=config.MODEL.VSSM.DEPTHS,
-            dims=config.MODEL.VSSM.EMBED_DIM,
-            # ===================
-            ssm_d_state=config.MODEL.VSSM.SSM_D_STATE,
-            ssm_ratio=config.MODEL.VSSM.SSM_RATIO,
-            ssm_rank_ratio=config.MODEL.VSSM.SSM_RANK_RATIO,
-            ssm_dt_rank=("auto" if config.MODEL.VSSM.SSM_DT_RANK == "auto" else int(config.MODEL.VSSM.SSM_DT_RANK)),
-            eq_tranNum=config.MODEL.VSSM.EQ_tranNum,            # 4
-            ssm_act_layer=config.MODEL.VSSM.SSM_ACT_LAYER,
-            ssm_conv=config.MODEL.VSSM.SSM_CONV,
-            ssm_conv_bias=config.MODEL.VSSM.SSM_CONV_BIAS,
-            ssm_drop_rate=config.MODEL.VSSM.SSM_DROP_RATE,
-            ssm_init=config.MODEL.VSSM.SSM_INIT,
-            forward_type=config.MODEL.VSSM.SSM_FORWARDTYPE,
-            # ===================
-            mlp_ratio=config.MODEL.VSSM.MLP_RATIO,
-            mlp_act_layer=config.MODEL.VSSM.MLP_ACT_LAYER,
-            mlp_drop_rate=config.MODEL.VSSM.MLP_DROP_RATE,
-            # ===================
-            drop_path_rate=config.MODEL.DROP_PATH_RATE,         # 0.2
-            patch_norm=config.MODEL.VSSM.PATCH_NORM,
-            norm_layer=config.MODEL.VSSM.NORM_LAYER,
-            downsample_version=config.MODEL.VSSM.DOWNSAMPLE,
-            patchembed_version=config.MODEL.VSSM.PATCHEMBED,
-            gmlp=config.MODEL.VSSM.GMLP,
-            use_checkpoint=config.TRAIN.USE_CHECKPOINT,
-            # ===================
-            posembed=config.MODEL.VSSM.POSEMBED,
-            imgsize=config.DATA.IMG_SIZE,
-        )
-        return model
 
     return None
 
 
 def build_model(config, is_pretrain=False):
-    model = None
-    if model is None:
-        model = build_vssm_model(config)
-    if model is None:
-        from .simvmamba import simple_build
-        model = simple_build(config.MODEL.TYPE)
+    model = build_vssm_model(config)
     return model
 
 
